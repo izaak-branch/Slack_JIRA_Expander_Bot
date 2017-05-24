54602dd1f13f50c0ebe5fb4c5256143ac9aed8aa
Izaak Branch <izaak.branch@tylertech.com>
2017-05-24 07:57:14 -0600

    Updated environment variable assignments: - Optional variable is now accessed safely instead of throwing a KeyError if unset - Added comments to note which variables are required and which are optional

--- JIRAbot.py
+++ JIRAbot.py
@@ -6,12 +6,12 @@ import time
 import re
 
 # Use environment variables for portability
-api_token = os.environ['SLACK_EXPANDER_API_TOKEN']
-channel = os.environ['SLACK_EXPANDER_CHANNEL']
-jira_base_url = os.environ['JIRA_BASE_URL']
-bot_regex_raw = os.environ['SLACK_EXPANDER_BOT_REGEX']
-bot_icon_url = os.environ['SLACK_EXPANDER_BOT_ICON']
-bot_username = 'JIRA Expander'
+api_token = os.environ['SLACK_EXPANDER_API_TOKEN'] # Required. If not set, will result in a KeyError
+channel = os.environ['SLACK_EXPANDER_CHANNEL'] # Required. If not set, will result in a KeyError
+jira_base_url = os.environ['JIRA_BASE_URL'] # Required. If not set, will result in a KeyError
+bot_regex_raw = os.environ['SLACK_EXPANDER_BOT_REGEX'] # Required. If not set, will result in a KeyError
+bot_icon_url = os.environ.get('SLACK_EXPANDER_BOT_ICON') # Optional. Not setting will result in bot having default picture
+bot_username = 'JIRA Expander' # Change if you want your bot to post under a different name
 
 sc = SlackClient(api_token)
 msgs = []
